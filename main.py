48adb850c89d4a1d1c0bda9bdc925e9c5af48ba2
Cloudiful <cloudiful@outlook.com>
2022-03-28 17:38:08 +0800

    update

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
 import gui
-import face
-import threading
 
 if __name__ == "__main__":
-
-    # tGUI = threading.Thread(target=gui.init, args=())
-    # tFace = threading.Thread(target=face.capture, args=())
-    #
-    # tGUI.start()
-    # tFace.start()
 
     gui.init()
 
