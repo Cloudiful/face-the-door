645400e6ca2ece42e197f3aee24cb428e434fd30
Cloudiful <cloudiful@outlook.com>
2022-03-28 17:37:34 +0800

    use thread to co-op with face.py

--- gui.py
+++ gui.py
@@ -1,8 +1,19 @@
+import pwdui
+import face
+import servo
+import threading
 from PySide2.QtWidgets import QApplication, QMainWindow, QDialog
 import data
 from indexui import Ui_MainWindow
 from pwdui import Ui_Dialog as Ui_PwdWindow
 from unlockui import Ui_Dialog as Ui_UnlockWindow
+from failedui import Ui_Dialog as Ui_FailedWindows
+
+
+def callFaceProcess():
+    if data.method == 'face':
+        tFace = threading.Thread(target=face.capture, args=())
+        tFace.start()
 
 
 class MainWindows(QMainWindow):
@@ -11,9 +22,12 @@ class MainWindows(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.pwdPage = None
+        callFaceProcess()
 
     def pwdPressed(self):
+        data.method = 'pwd'
         print('Goto password input page')
+        data.pwd = ''
         self.pwdPage = PwdWindow()
         self.pwdPage.showFullScreen()
 
@@ -21,19 +35,41 @@ class MainWindows(QMainWindow):
 class PwdWindow(QDialog):
     def __init__(self):
         super(PwdWindow, self).__init__()
+        self.failedPage = None
+        self.unlockPage = None
         self.ui = Ui_PwdWindow()
         self.ui.setupUi(self)
 
     def passwordInput(self, i):
-        if len(data.pwd) < 6:
-            if type(i) == int:
-                data.pwd += str(i)
-            elif i == 'yes':
-                pass
-            elif i == 'back' and len(data.pwd) > 0:
-                data.pwd = data.pwd[:-1]
+        if type(i) == int and len(data.pwd) < 6:
+            data.pwd += str(i)
+        elif i == 'yes':
+            if data.pwd == '123456':
+                self.unlockPage = UnlockWindows()
+                self.unlockPage.showFullScreen()
+                data.pwd = ''
+                unlockThread = threading.Thread(target=servo.unlock, args=())
+                unlockThread.start()
+                pwdui.QTimer.singleShot(7000, lambda: self.unlockPage.close())
+
+                data.method = 'face'
+                callFaceProcess()
+                self.close()
+
+            else:
+                self.failedPage = FailedWindows()
+                self.failedPage.showFullScreen()
+                data.pwd = ''
+
+        elif i == 'back' and len(data.pwd) > 0:
+            data.pwd = data.pwd[:-1]
 
         self.ui.textEdit.setText(data.pwd)
+
+    def backfrompwd(self):
+        data.method = 'face'
+        callFaceProcess()
+        self.close()
 
     def b0(self):
         self.passwordInput(0)
@@ -68,14 +104,21 @@ class PwdWindow(QDialog):
     def byes(self):
         self.passwordInput('yes')
 
-    def bback(self):
+    def back(self):
         self.passwordInput('back')
 
 
 class UnlockWindows(QDialog):
     def __init__(self):
         super(UnlockWindows, self).__init__()
-        self.ui = Ui_UnlockWindow
+        self.ui = Ui_UnlockWindow()
+        self.ui.setupUi(self)
+
+
+class FailedWindows(QDialog):
+    def __init__(self):
+        super(FailedWindows, self).__init__()
+        self.ui = Ui_FailedWindows()
         self.ui.setupUi(self)
 
 
