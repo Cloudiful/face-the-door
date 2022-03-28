423eb04cd5ee42cada3b7b3d327db1f38e5b9dfe
Cloudiful <cloudiful@outlook.com>
2022-03-28 14:50:02 +0800

    GUI logic

--- gui.py
+++ gui.py
@@ -1,10 +1,8 @@
 from PySide2.QtWidgets import QApplication, QMainWindow, QDialog
-
+import data
 from indexui import Ui_MainWindow
 from pwdui import Ui_Dialog as Ui_PwdWindow
 from unlockui import Ui_Dialog as Ui_UnlockWindow
-
-password = ''
 
 
 class MainWindows(QMainWindow):
@@ -25,6 +23,53 @@ class PwdWindow(QDialog):
         super(PwdWindow, self).__init__()
         self.ui = Ui_PwdWindow()
         self.ui.setupUi(self)
+
+    def passwordInput(self, i):
+        if len(data.pwd) < 6:
+            if type(i) == int:
+                data.pwd += str(i)
+            elif i == 'yes':
+                pass
+            elif i == 'back' and len(data.pwd) > 0:
+                data.pwd = data.pwd[:-1]
+
+        self.ui.textEdit.setText(data.pwd)
+
+    def b0(self):
+        self.passwordInput(0)
+
+    def b1(self):
+        self.passwordInput(1)
+
+    def b2(self):
+        self.passwordInput(2)
+
+    def b3(self):
+        self.passwordInput(3)
+
+    def b4(self):
+        self.passwordInput(4)
+
+    def b5(self):
+        self.passwordInput(5)
+
+    def b6(self):
+        self.passwordInput(6)
+
+    def b7(self):
+        self.passwordInput(7)
+
+    def b8(self):
+        self.passwordInput(8)
+
+    def b9(self):
+        self.passwordInput(9)
+
+    def byes(self):
+        self.passwordInput('yes')
+
+    def bback(self):
+        self.passwordInput('back')
 
 
 class UnlockWindows(QDialog):
