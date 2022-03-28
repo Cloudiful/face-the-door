5b6176b9fd69456b53dc3daa64017fe989991924
Cloudiful <cloudiful@outlook.com>
2022-03-28 14:50:24 +0800

    main program

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ import threading
 
 if __name__ == "__main__":
 
-    tGUI = threading.Thread(target=gui.init,args=())
-    tFace = threading.Thread(target=face.capture,args=())
+    # tGUI = threading.Thread(target=gui.init, args=())
+    # tFace = threading.Thread(target=face.capture, args=())
+    #
+    # tGUI.start()
+    # tFace.start()
 
-    tGUI.start()
-    tFace.start()
+    gui.init()
 
     a = input('a')
