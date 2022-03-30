ccd0edde051a03e5a72caaabf15f371ce0e5969b
Cloudiful <cloudiful@outlook.com>
2022-03-30 16:08:51 +0800

    batch process for ui2py

---
+++ ui2pybatch.bat
@@ -0,0 +1,6 @@
+pyside2-uic.exe .\index.ui -o indexui.py
+pyside2-uic.exe .\pwdPage.ui -o pwdui.py
+pyside2-uic.exe .\failedPage.ui -o failedui.py
+pyside2-uic.exe .\unlockPage.ui -o unlockui.py
+pyside2-uic.exe .\loading.ui -o loadingui.py
+pyside2-uic.exe .\failedPageFace.ui -o failedfaceui.py
